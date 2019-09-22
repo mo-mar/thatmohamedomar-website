939cedfd5f326fae443dfec86214670b8baea192
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-22 17:31:58 -0400

    added images for recent work examples

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
-myApp.init = function(){
+myApp = {};
 
+myApp.revealWork = function(){
+    $('#under-the-sea').on('click touch', function(){
+    $('.under-the-sea-example').toggleClass("reveal-example");
+    })
+}
+
+myApp.revealHamburger = function(){
+    $('nav label').on('click touch', function(){
+        // $(".hamburger-toggle").hide();
+        $('.hamburger-menu').toggleClass("reveal-menu");
+    })
+}
+myApp.init = function(){
+    myApp.revealWork();
+    myApp.revealHamburger();
 }
 
 $(document).ready(function(){
