7b78c2b409d827efdff665996b0f706320f2f619
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-24 15:43:06 -0400

    added some changes based on group feedback

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 myApp = {};
 
 
-myApp.revealWork = function(){
-    $('#under-the-sea').on('click touch', function(){
-    $('.under-the-sea-example').toggleClass("reveal-example");
-    })
-    $('#game-night').on('click touch', function () {
-        $('.game-night-example').toggleClass("reveal-example");
-    })
-}
-
 myApp.flickity = function(){
     $('.recent-work-master').flickity({
         // options
@@ -36,7 +27,6 @@ myApp.revealHamburger = function(){
     })
 }
 myApp.init = function(){
-    myApp.revealWork();
     myApp.revealHamburger();
     myApp.flickity();
     myApp.smoothScroll();
